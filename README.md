4d55e6c98035d0f709ef84713ad32fc75e0fc54b
epdef1 <erik.defreitas@stu.ocsb.ca>
2025-11-25 19:39:39 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ICS3C Template
+
+This is the template repo for all new repos in the ICS3C course.
---
+++ main.c
@@ -0,0 +1,14 @@
+// Copyright (c) 2025 St. Mother Teresa HS All rights reserved.
+//
+// Created by: Mr. Coxall
+// Created on: Sep 2025
+// This program displays, "Hello, World!"
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello, World!\n");
+
+    printf("\nDone.")
+    return 0;
+}
